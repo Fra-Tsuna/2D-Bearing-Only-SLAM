8c8630fb82aedbdd6e4875012c683e65a47c8060
Francesco Argenziano <francescoarg@live.it>
2022-01-21 18:57:38 +0100

    gt data parsed
    gt data parsed

--- BearingOnlySlam2D.m
+++ BearingOnlySlam2D.m
@@ -34,11 +34,50 @@ end
 
 state_dim = 3*Nr + 2*Nl;
 
-#init measurements
+#find total number of measurements
 Ml=0;
 for i=1:length(obs_gt)
     m=length(obs_gt(i).observation);
     Ml += m;
 end
 
+#init bearing measurements
 Zl=zeros(Ml);
+associations_Zl = zeros(2,Ml);
+
+#create for each bearing measurement an association between the pose that recorded it
+#and the landmark that originated it while extracting them
+m=1;
+for i=1:length(obs_gt)
+    observations = obs_gt(i);
+    pose_id = observations.pose_id;
+    pose_index = find(pose_id2index==pose_id);
+    num_observations = length(observation.observations);
+    for j=1:num_observations
+        measurement = observations.observation(j);
+        land_id = measurement.id;
+        land_index = find(land_id2index==land_id);
+        Zl(m)=measurement.bearing;
+        associations_Zl(:,m) = [pose_index; land_index];
+        m++;
+    end
+end
+
+#init odometry measurements
+Mr = length(trans_gt);
+Zr = zeros(3,3,Mr);
+associations_Zr = zeros(2,Mr);
+
+
+#create for each odometry measurement an association between the previous pose and
+#the subsequent pose
+m=1;
+for i=1:Mr
+    transition = trans_gt(i);
+    pose_from_id = transition.id_from;
+    pose_to_id = transition.id_to;
+    pose_from_index = find(pose_id2index==pose_from_id);
+    pose_to_index = find(pose_id2index==pose_to_id);
+    associations_Zr(:,i) = [pose_from_index; pose_to_index];
+    Zr(:,:,i) = v2t(transition(i).v);
+end
